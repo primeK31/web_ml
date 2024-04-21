4cd3837d948161b06dd6a3a9365dbd312ba90a3c
urchocoeyes <nazymtolebek@gmail.com>
2024-04-21 16:03:32 +0600

    allowed-localhost upd

--- backend/backend/mlforces/mlforces/settings.py
+++ backend/backend/mlforces/mlforces/settings.py
@@ -59,6 +59,10 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
